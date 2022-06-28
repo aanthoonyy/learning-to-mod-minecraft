2e69ae038c022c3a0e22120bad51232b48cb5836
aanthoonyy <53194069+aanthoonyy@users.noreply.github.com>
2022-06-27 19:49:39 -0700

    global loot tables

---
+++ src/main/java/com/anthony/tutorialmod/event/ModEventBusEvents.java
@@ -0,0 +1,29 @@
+package com.anthony.tutorialmod.event;
+
+import com.anthony.tutorialmod.TutorialMod;
+import com.anthony.tutorialmod.event.loot.CoalCokeFromCreeperAdditionModifier;
+import com.anthony.tutorialmod.event.loot.CucumberSeedsFromGrassAdditionModifier;
+import com.anthony.tutorialmod.event.loot.DowsingRodInIglooAdditionModifier;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.common.loot.GlobalLootModifierSerializer;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+import javax.annotation.Nonnull;
+
+@Mod.EventBusSubscriber(modid = TutorialMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class ModEventBusEvents {
+    @SubscribeEvent
+    public static void registerModifierSerializers(@Nonnull final RegistryEvent.Register<GlobalLootModifierSerializer<?>>
+                                                           event) {
+        event.getRegistry().registerAll(
+                new CucumberSeedsFromGrassAdditionModifier.Serializer().setRegistryName
+                        (new ResourceLocation(TutorialMod.MOD_ID,"cucumber_seeds_from_grass")),
+                new DowsingRodInIglooAdditionModifier.Serializer().setRegistryName
+                        (new ResourceLocation(TutorialMod.MOD_ID,"dowsing_rod_in_igloo")),
+                new CoalCokeFromCreeperAdditionModifier.Serializer().setRegistryName
+                        (new ResourceLocation(TutorialMod.MOD_ID,"coal_coke_from_creeper"))
+        );
+    }
+}
